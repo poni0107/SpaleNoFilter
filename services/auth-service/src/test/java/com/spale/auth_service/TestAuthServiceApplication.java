073a0df3e52144da99c85a09fd0358225c76b7bf
Marijana Jeremić <zvezdanajeremic74@gmail.com>
2026-02-09 02:35:15 +0100

    feat(auth): initial backend A files

---
+++ services/auth-service/src/test/java/com/spale/auth_service/TestAuthServiceApplication.java
@@ -0,0 +1,11 @@
+package com.spale.auth_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(AuthServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
